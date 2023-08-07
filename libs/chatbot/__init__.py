7bc59a5acdc51fc777a338b25eeaf986730d90d4
Jeremy.Huang <jeremy@teamt5.org>
2023-08-07 16:03:10 +0800

    :construction: Move chatGPT model options to confogs

--- libs/chatbot/__init__.py
+++ libs/chatbot/__init__.py
@@ -8,15 +8,10 @@ class ChatGPT_Object:
     @classmethod
     def chat_completion(cls, messages):
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model=configs.CHATGPT_USE_MODEL,
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": f"請使用繁體中文回答，{messages}"},
             ],
         )
         return response.choices[0].message.content.strip()
-
-
-if __name__ == "__main__":
-    message = "請編寫一篇文章，主題是（ChatGPT 與自媒體結合，提升品牌行銷之運用）。其中包含（Ai科技）、（市場火爆）、（多元化）"
-    print(ChatGPT_Object.chat_completion(message))
--- libs/configs/__init__.py
+++ libs/configs/__init__.py
@@ -4,9 +4,10 @@ from linebot import LineBotApi, WebhookHandler
 import os
 from libs.configs.__version__ import __version__
 
-
+# read .env file
 load_dotenv()
 
+# *----- piNews Server Setting -----*
 DEBUG = os.getenv("DEBUG")
 DOCS_URL = "/PiNews_LineBot/Swagger_UI"
 REDOC_URL = "/PiNews_LineBot/API_redoc"
@@ -15,14 +16,18 @@ VERSION = __version__
 HOST = "0.0.0.0"
 PORT = 8000
 DESCRIPTION = """
-## PiNews_LineBot
-# """
 
+## PiNews_LineBot
+
+"""
+
+# *----- Line Bot Setting -----*
 CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
-
 LINEBOT_API = LineBotApi(CHANNEL_ACCESS_TOKEN)
 LINEBOT_HANDLER = WebhookHandler(CHANNEL_SECRET)
 
 
+# *----- OpenAI (ChatGPT) Setting -----*
 OPEN_API_KEY = os.getenv("OPEN_API_KEY")
+CHATGPT_USE_MODEL = "gpt-3.5-turbo"
