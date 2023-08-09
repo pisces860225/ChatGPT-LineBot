42dce17f06c5baafc7a18ceb8d2e8fe0b6bf9165
Jeremy.Huang <jeremy@teamt5.org>
2023-08-09 15:06:57 +0800

    :bento: Created env get method

--- libs/configs/__init__.py
+++ libs/configs/__init__.py
@@ -6,12 +6,25 @@ from linebot import LineBotApi, WebhookHandler
 
 from libs.configs.__version__ import __version__
 
+
 # read .env file
-ENV_FILE_PATH = os.path.join(os.getcwd().replace("\\", "/"), ".env")
-load_dotenv(ENV_FILE_PATH)
+def get_env_variable(key):
+    try:
+        # Try getting from os environment first
+        value = os.environ[key]
+    except KeyError:
+        # If not present, load .env and try again
+        try:
+            ENV_FILE_PATH = os.path.join(os.getcwd().replace("\\", "/"), ".env")
+            load_dotenv(ENV_FILE_PATH)
+            value = os.environ[key]
+        except KeyError:
+            value = None
+    return value
+
 
 # *----- piNews Server Setting -----*
-DEBUG = os.getenv("DEBUG")
+DEBUG = get_env_variable("DEBUG")
 DOCS_URL = "/PiNews_LineBot/Swagger_UI"
 REDOC_URL = "/PiNews_LineBot/API_redoc"
 SERVERTITLE = "PiNews_LineBot"
@@ -25,15 +38,15 @@ DESCRIPTION = """
 """
 
 # *----- Line Bot Setting -----*
-CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
-CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
+CHANNEL_ACCESS_TOKEN = get_env_variable("CHANNEL_ACCESS_TOKEN")
+CHANNEL_SECRET = get_env_variable("CHANNEL_SECRET")
 LINEBOT_API = LineBotApi(CHANNEL_ACCESS_TOKEN)
 LINEBOT_HANDLER = WebhookHandler(CHANNEL_SECRET)
 
 
 # *----- OpenAI (ChatGPT) Setting -----*
 OPENAI = openai
-OPENAI.api_key = os.getenv("OPENAI_API_KEY")
+OPENAI.api_key = get_env_variable("OPENAI_API_KEY")
 OPENAI_USE_MODEL = "gpt-3.5-turbo"
 
 
